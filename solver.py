a3ea20f366fa92708489c19dfab711ff99410c03
ihamzazam <hamzazam00@gmail.com>
2020-12-25 15:46:15 +0800

    validation function

--- solver.py
+++ solver.py
@@ -23,9 +23,31 @@ def print_board(bo):
                 print(str(bo[i][j]) + " " , end="")
 
                 
-def find-empty(board)
+def find_empty(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
-            if board[i][j] == 0;
+            if board[i][j] == 0:
                 return(i, j)
-    return none
+    return None
+
+def validity(board, num, pos):
+    # row validation
+    for i in range(len(board[0])): # iteration through the first list as reference
+        if board[pos[0]][i] == num and pos[1] != i: # check the whole row excluding current pos
+            return False
+
+    # column validation
+    for i in range(len(board)): # iteration through the 2D list
+        if board[i][pos[0]] == num and pos[0] != i: # check the column excluding current pos
+            return False
+
+    # box validator
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3): # iteration through all three vertical boxes
+        for j in range(box_x * 3, box_x * 3 + 3): # iteration through all three horizontal boxes
+            if board[i][j] == num and (i, j) != pos: # check the box for same number excluding the pos
+                return False
+    return True
+
